b8cbf6278a8e84d6fd6253aeabe9a1e99e790794
josseandrofarias <farias.josseandro@gmail.com>
2019-09-07 19:40:27 -0400

    Index da api, com todo controle de consign

---
+++ index.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const server = require( path.resolve( 'src', 'config', 'server.js')) //start server
+
+const db = require(path.resolve( 'src', 'config', 'db.js'))
+const consign = require('consign')
+
+consign()
+    .then(path.resolve('/src', 'config', 'middlewares.js'))
+    .then(path.resolve('/src', 'controllers'))
+    .then(path.resolve('/src', 'routes', 'principal.js'))
+    .into(server)
+
+server.db = db
+
+!server ? console.log('Erro ao iniciar server API' ) : console.log('API iniciada')
+
+
+
+
+
