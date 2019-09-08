8e986c2dc18f158d79516a0674aeca77c1ab428f
josseandrofarias <farias.josseandro@gmail.com>
2019-09-08 17:17:19 -0400

    Arquivo base para o CRUD de pedido

---
+++ src/controllers/Pedido.js
@@ -0,0 +1,27 @@
+const moment = require('moment')
+
+module.exports = app => {
+    const getPedido = (req, res) => {
+
+    }
+
+    const save = (req, res) => {
+
+    }
+
+    const remove = (req, res) => {
+
+    }
+
+    const update = (req, res, doneAt) => {
+
+    }
+
+    return {
+        getPedido,
+        save,
+        remove,
+        update
+    }
+
+}
