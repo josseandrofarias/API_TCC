41088f7ecac9b44c5b4f0f396f978c9341dda3c6
josseandrofarias <farias.josseandro@gmail.com>
2019-09-08 17:14:17 -0400

    CRUD da tabela Empresa

--- src/controllers/Empresa.js
+++ src/controllers/Empresa.js
@@ -1,29 +1,30 @@
 const moment = require('moment')
 
 module.exports = app => {
-    const getEmpresa = (req, res) => {
+    const getempresa = (req, res) => {
         app.db('empresa')
-            .where({id: req.params.id, status: true})
+            .innerJoin('pessoa', 'pessoa.id', 'empresa.id_pessoa')
+            .innerJoin('setor', 'setor.id', 'empresa.id_setor')
+            .where({id: req.params.id})
             .then(empresa => res.json(empresa))
             .catch(err => rest.status(500).json(err))
     }
 
-    const getEmpresas = (req, res) => {
+    const getempresas = (req, res) => {
         app.db('empresa')
+            .innerJoin('empresa', 'empresa.id', 'empresa.id_empresa')
             .where({status: true})
-            .orderBy('nome')
+            .orderBy('empresa.nome')
             .then(empresa => res.json(empresa))
             .catch(err => rest.status(500).json(err))
     }
 
     const save = (req, res) => {
-        if(!req.body.nome.trim()){
-            return res.status(400).send('Nome é um campo obrigatório')
-        }
+        if(!req.body.nome.trim()) return res.status(400).send('Nome é um campo obrigatório!')
 
         app.db('empresa')
             .insert(req.body)
-            .then(_ => res.status(204).send('Empresa Cadastrada!'))
+            .then(_ => res.status(204).send('empresa Cadastrada!'))
             .catch(err => res.status(400).json(err))
     }
 
@@ -32,10 +33,9 @@ module.exports = app => {
             .where({id: req.params.id})
             .del()
             .then(rowsDeleted => {
-                if(rowsDeleted > 0){
-                    res.status(204).send()
-                }else{
-                    const msg = `Não foi encontrada empresa com id ${req.params.id}`
+                if(rowsDeleted > 0) res.status(204).send()
+                else{
+                    const msg = `Não foi encontrado empresa com id ${req.params.id}!`
                     res.status(400).send(msg)
                 }
             })
@@ -43,8 +43,7 @@ module.exports = app => {
     }
 
     const update = (req, res, obj = null) => {
-        if(obj == null)
-            obj = req.params
+        if(obj == null) obj = req.params
 
         app.db('empresa')
             .where({id: req.params.id})
@@ -59,7 +58,7 @@ module.exports = app => {
             .first()
             .then(empresa => {
                 if(!empresa){
-                    const msg = `Empresa com id ${req.params.id} não encontrada`
+                    const msg = `empresa com id ${req.params.id} não encontrado!`
                     res.status(400).send(msg)
                 }
                 const status = empresa.status != req.params.status ?  req.params.status : null
@@ -72,8 +71,8 @@ module.exports = app => {
     }
 
     return {
-        getEmpresa,
-        getEmpresas,
+        getempresa,
+        getempresas,
         save,
         remove,
         update,
