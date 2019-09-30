2fa694d5c45fffb5f08fd559935b767d8c98ea98
josseandrofarias <farias.josseandro@gmail.com>
2019-09-29 20:54:02 -0400

    Add routa e crud pedido

--- src/controllers/Pedido.js
+++ src/controllers/Pedido.js
@@ -2,20 +2,46 @@ const moment = require('moment')
 
 module.exports = app => {
     const getPedido = (req, res) => {
-
+        app.db('pedido')
+            .innerJoin('pessoa', 'pessoa.id', 'pedido.id_pessoa')
+            .innerJoin('setor', 'setor.id', 'pedido.id_setor')
+            .leftJoin('imagem', 'imagem.id_pedido', 'pedido.id')
+            .where({id: req.params.id})
+            .then(pedido => res.json(pedido))
+            .catch(err => rest.status(500).json(err))
     }
 
     const save = (req, res) => {
-
+          app.db('pedido')
+            .insert(req.body)
+            .then(_ => res.status(204).send('pedido Cadastrada!'))
+            .catch(err => res.status(400).json(err))
     }
 
     const remove = (req, res) => {
-
+        app.db('pedido')
+            .where({id: req.params.id})
+            .del()
+            .then(rowsDeleted => {
+                if(rowsDeleted > 0) res.status(204).send()
+                else{
+                    const msg = `Não foi encontrado pedido com id ${req.params.id}!`
+                    res.status(400).send(msg)
+                }
+            })
+            .catch(err => res.status(400).json(err))
     }
 
-    const update = (req, res, doneAt) => {
+    const update = (req, res, obj = null) => {
+        if(obj == null) obj = req.params
 
+        app.db('pedido')
+            .where({id: req.params.id})
+            .update(obj)
+            .then(_ => res.status(204).send('Dados Atualizados!'))
+            .catch(err => res.status(400).json(err))
     }
+
 
     return {
         getPedido,
--- src/routes/principal.js
+++ src/routes/principal.js
@@ -8,17 +8,18 @@ module.exports = app => {
     app.post('/cadastrar', upload.single('avatar'), app.src.controllers.Pessoa.save)
     app.post('/login', app.src.controllers.auth.login)
 
-    app.route('/empresa')
-        .all(app.src.config.passport.authenticate())
-        .get(app.src.controllers.Empresa.getEmpresas)
-        .post(app.src.controllers.Empresa.save)
-
-    app.route('/empresa/:id') //:id é um parametro
-        .all(app.src.config.passport.authenticate())
-        .get(app.src.controllers.Empresa.getEmpresa)
-        .delete(app.src.controllers.Empresa.remove)
-
-    app.route('/empresa/:id/update') //:id é um parametro
-        .all(app.src.config.passport.authenticate())
-        .put(app.src.controllers.Empresa.update)
+    app.post('/pedido', app.src.controllers.Pedido.save)
+    // app.route('/empresa')
+    //     .all(app.src.config.passport.authenticate())
+    //     .get(app.src.controllers.Empresa.getEmpresas)
+    //     .post(app.src.controllers.Empresa.save)
+    //
+    // app.route('/empresa/:id') //:id é um parametro
+    //     .all(app.src.config.passport.authenticate())
+    //     .get(app.src.controllers.Empresa.getEmpresa)
+    //     .delete(app.src.controllers.Empresa.remove)
+    //
+    // app.route('/empresa/:id/update') //:id é um parametro
+    //     .all(app.src.config.passport.authenticate())
+    //     .put(app.src.controllers.Empresa.update)
 }
